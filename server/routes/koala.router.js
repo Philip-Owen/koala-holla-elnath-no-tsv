65c41d06d4dc796408e3b8aecd9536809e04309f
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-10 13:15:13 -0600

    completed get/post routes on koala router and server.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,5 +4,29 @@ const pool = require('..modules/pool');
 
 
 app.get('/', (req, res) =>{
-    const queryText = 'SELECT * FROM koala'
+    const queryText = 'SELECT * FROM koala';
+    pool.queryText(queryText)
+        .then((result) => {
+            console.log('query results: ', result);            
+            res.send(result.rows);
+        })
+        .catch((err) => {
+            console.log('error making select query:', err);
+            res.sendStatus(500);
+        });
 });
+
+app.post('/', (req, res) => {
+    const queryText = 'INSERT INTO koala("name", "gender", "age", "ready_to_transfer", "notes") VALUES($1, $2, $3, $4, $5)';
+    pool.queryText(queryText, [req.body.name, req.body.gender, req.body.age, req.body.ready_to_transfer, req.body.notes])
+        .then((result) => {
+            console.log('query results: ', result);
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('error making insert query:', err);
+            res.sendStatus(500);
+        });
+});
+
+module.exports = router;
