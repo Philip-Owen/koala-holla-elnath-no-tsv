7a1b81869858cf83c8b249ac118dd511601f9a47
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-10 10:59:02 -0600

    configured pool.js file

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('..modules/pool');
+
+
+app.get('/', (req, res) =>{
+    const queryText = 'SELECT * FROM koala'
+});
