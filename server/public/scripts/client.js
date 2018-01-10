b9a2479f2283bb22ca9316fe0d891f5c2f4aad32
Ross <ross@tests-MacBook-Pro.local>
2018-01-10 10:58:38 -0600

    Changed some inputs and made selects in html, added functions in client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,44 @@ $( document ).ready(onReady());
 
 function onReady(){
   console.log('JQ');
-  
+  getKoala();
+  $('#addButton').on('click', addKoala);
+}
+
+function getKoala(){
+  $.ajax({
+    method: 'GET',
+    url: '/koala',
+    success: function(response){
+      console.log('get request response', response);
+      
+    }
+  });
+}
+
+function addKoala(){
+  let newKoala = {
+    name: $('#nameIn').val(),
+    age: $('#ageIn').val(),
+    gender: $('#gender').val(),
+    ready_to_transfer: $('#ready_to_transfer').val(),
+    notes: $('#notesIn').val()
+  }
+  $.ajax({
+    method: 'POST',
+    url: '/koala',
+    method: newKoala,
+    success: function(response){
+      getKoala();
+      resetInput();
+    }
+  })
+}
+
+function resetInput(){
+  $('#nameIn').val('');
+  $('#ageIn').val('');
+  $('#gender').val('');
+  $('#ready_to_transfer').val('');
+  $('#notesIn').val('');
 }
