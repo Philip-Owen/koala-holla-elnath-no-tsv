e49dd31b72206d06b40c89e249cd1a6a234b87f7
Alaethia <truthcharis@gmail.com>
2018-01-11 09:21:57 -0600

    added comments to server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
+// *********************************************************
+//                      Dependencies 
+// *********************************************************
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 5000;
+const koalaRouter = require('./routes/koala.router');
+
+// *********************************************************
+//                      Middleware 
+// *********************************************************
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-const koalaRouter = require('./routes/koala.router');
-
+//Routes 
 app.use('/koala', koalaRouter);
 
+// *********************************************************
+//                      Server Listener 
+// *********************************************************
+
+var port = process.env.PORT || 5000;
 // Start listening for requests on a specific port
 app.listen(port, function(){
   console.log('listening on port', port);
